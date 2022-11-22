3ede2f98bdfcc9f0720ace6f887119bf4757a036
Anna Velarde <annavelardevilaseca@gmail.com>
2022-11-22 20:35:28 +0000

    add mongodb to the server

---
+++ Server/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+if (process.env.REACT_ENV !== "production") {
+  mongoose.set("debug", true);
+}

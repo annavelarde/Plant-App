3af655aa3d35b269f9750da137443c3925c5135f
Anna Velarde <annavelardevilaseca@gmail.com>
2023-03-09 08:57:04 +0000

    working on delete profile and edit profile

--- Client/src/pages/UpdateProfile/UpdateProfile.jsx
+++ Client/src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,9 +1,160 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  updateProfileImage,
+  updatingUserName,
+} from "../../services/userService";
+import * as PATHS from "../../utils/paths";
+import "./UpdateProfile.css";
 
-function UpdateProfile() {
+const blancForm = {
+  username: "",
+  email: "",
+  password: "",
+  userDescription: "",
+  profileImage: "",
+};
+
+function UpdateProfile(props) {
+  const { user, setUser } = props;
+  // console.log(" 👉 👉 / UpdateProfile / props:", props);
+  // console.log(" 👉 👉 / UpdateProfile / setUser:", setUser);
+  // console.log(" 👉 👉 / UpdateProfile / user:", user);
+
+  // const [username, setUsername] = useState(user.username);
+  // const [email, setEmail] = useState(user.email);
+  // const [country, setCountry] = useState(user.country);
+  // const [password, setPassword] = useState(user.password);
+
+  const [chosenPicture, setChosenPicture] = useState("");
+  const [inputKey, setInputKey] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const [infoUser, setInfoUser] = useState(blancForm);
+  // console.log(" 👉 👉 / UpdateProfile / infoUser:", infoUser);
+
+  //main form
+  function handleFromSubmit(event) {
+    event.preventDefault();
+    setError(false);
+
+    // if (!chosenPicture || !username || !email || !country) {
+    //   setError("Please, check before saving!");
+    //   return;
+    // }
+
+    // const formBody = new window.FormData();
+    // formBody.append("profileImage", chosenPicture);
+    // formBody.append("userId", user._id);
+
+    updateProfileImage(infoUser)
+      .then((res) => {
+        if (!res.success) {
+          setError(response.data);
+          setIsLoading(false);
+          return;
+        }
+        // setUser({ ...user, profileImage: res.data.profileImage });
+        setUser(response.data.user);
+        console.log(
+          " 👉 👉 / updateProfileImage / response:",
+          response.data.user
+        );
+      })
+      .finally(() => {
+        setIsLoading(false);
+        // setInputKey(Date.now());
+      });
+  }
+  //Image
+  function handleImageInput(event) {
+    console.log(event.target.files[0]);
+    const imageFromInput = event.target.files[0];
+
+    setChosenPicture(imageFromInput);
+  }
+  //Text
+  function handleTextChange(e) {
+    const { name, value } = e.target;
+    setInfoUser({ ...infoUser, [name]: value });
+    console.log(" 👉 👉 / handleTextChange / setInfoUser:", setInfoUser);
+  }
+
   return (
-    <div>
-      <h1>Update profile</h1>
+    <div className="card-body">
+      <h1> Hello {user.username}, </h1>
+      <b>update your profile below please</b>
+      <br />
+      <div className="card-container">
+        <img
+          className="round"
+          width="35%"
+          height="auto"
+          src={user.profileImage}
+          alt={isLoading ? user.chosenPicture : user.profileImage}
+        />
+        <div>
+          {error && <p style={{ color: "teal", fontWeight: "530" }}>{error}</p>}
+          <form onSubmit={handleFromSubmit} method="POST">
+            <div>
+              <div>
+                <input key={inputKey} type="file" onChange={handleImageInput} />
+                {/* <button type="submit">Update image</button> */}
+              </div>
+              <div>
+                <b> Name: </b>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder={user.username}
+                  value={infoUser.username}
+                  onChange={handleTextChange}
+                />
+                <b>Email:</b>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder={user.email}
+                  value={infoUser.email}
+                  onChange={handleTextChange}
+                />
+                <b>Country:</b>
+                <input
+                  type="text"
+                  name="country"
+                  placeholder={user.country}
+                  value={infoUser.country}
+                  onChange={handleTextChange}
+                />
+              </div>
+            </div>
+            <p>
+              Full Stack <br /> developer
+            </p>
+            <div className="buttons">
+              <button className="primary">Message</button>
+              <button className="primary ghost">Following</button>
+            </div>
+            <div className="skills">
+              {/* <h6>Skills/Tags</h6>
+              <ul>
+                <li>Front End Development</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node</li>
+              </ul> */}
+              <br />
+              <button className="primary btn btn-secondary mb-4">
+                Publish
+              </button>
+            </div>{" "}
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- Server/routes/user.routes.js
+++ Server/routes/user.routes.js
@@ -1,3 +1,138 @@
 const router = require("express").Router();
+const upload = require("../middleware/cloudinary");
+const User = require("../models/User.model");
+const Session = require("../models/Session.model");
+const Post = require("../models/Post.model");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+//updating image
+router.put(
+  "/updateProfileImage",
+  isLoggedIn,
+  upload.single("profileImage"),
+  (req, res) => {
+    const { userId } = req.body;
+    console.log(" 👉 👉 / body:", req.body);
+
+    User.findByIdAndUpdate(
+      userId,
+      { profileImage: req.file.path },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        res.json({
+          success: true,
+          profileImage: updatedUser.profileImage,
+        });
+      })
+      .catch((err) => {
+        res.json({
+          success: false,
+          message: "Server error uploading image",
+        });
+      });
+  }
+);
+
+//updating profile
+router.put("/edit-profile", isLoggedIn, (req, res) => {
+  const { username, email, country } = req.body;
+  console.log(" 👉 👉 / router.patch / req:", req.body);
+  const { _id } = req.user;
+  console.log(" 👉 👉 / router.patch / user:", req.user);
+
+  //updating username
+  //   if (username === _id.username) {
+  //     return res.json({ user: req.user });
+  //   }
+
+  //   User.findOne({ username }).then((foundedUser) => {
+  //     if (foundedUser) {
+  //       return res
+  //         .status(400)
+  //         .json({ errorMessage: "Username already exit. Insert another one" });
+  //     }
+
+  //     User.findByIdAndUpdate(_id, { username }, { new: true }).then(
+  //       (updatedUser) => {
+  //         console.log(" 👉 👉 / User.findOne / updatedUser:", updatedUser);
+  //         res.json({ user: updatedUser });
+  //       }
+  //     );
+  //   });
+
+  //   //updating email
+  //   if (email === _id.email) {
+  //     return res.json({ user: req.user });
+  //   }
+
+  //   User.findOne({ email }).then((foundedUser) => {
+  //     if (foundedUser) {
+  //       return res
+  //         .status(400)
+  //         .json({ errorMessage: "Email already exit. Insert another one" });
+  //     }
+
+  //     User.findByIdAndUpdate(_id, { email }, { new: true }).then(
+  //       (updatedUser) => {
+  //         console.log(" 👉 👉 / User.findOne / updatedUser:", updatedUser);
+  //         res.json({ user: updatedUser });
+  //       }
+  //     );
+  //   });
+
+  //updating country
+  //   if (country === _id.country) {
+  //     return res.json({ user: req.user });
+  //   }
+
+  //   User.findOne({ password }).then((foundedUser) => {
+  //     if (foundedUser) {
+  //       return res
+  //         .status(400)
+  //         .json({ errorMessage: "Country already exit. Insert another one" });
+  //     }
+
+  //     User.findByIdAndUpdate(_id, { country }, { new: true }).then(
+  //       (updatedUser) => {
+  //         console.log(" 👉 👉 / User.findOne / updatedUser:", updatedUser);
+  //         res.json({ user: updatedUser });
+  //       }
+  //     );
+});
+
+//delete profile
+router.delete("/:userId", isLoggedIn, async (req, res) => {
+  const { userId } = req.params;
+  console.log(" 👉 👉 / router.delete / userId:", userId);
+
+  //deleting all Posts
+  const userPosts = await Post.find({ author: userId });
+  const allPosts = userPosts.map((thePost) => thePost._id);
+  //   console.log(" 👉 👉 / router.delete / allPosts:", allPosts);
+  //   console.log(" 👉 👉 / router.delete / userPosts:", userPosts);
+
+  //deleting Session
+  const userSessionId = req.headers.authorization;
+  //   console.log(" 👉 👉 / router.delete / userSession:", userSession);
+
+  //deleting User
+  const deleteUser = await Session.findById(userSessionId).populate("user");
+  //   console.log(" 👉 👉 / router.delete / deleteUser:", deleteUser);
+
+  if (deleteUser.user._id.toString() !== userId) {
+    // console.log("We are here--", deleteUser.user._id.toString());
+    return res.status(404).json({
+      errorMessage: "Server Error deleting post",
+    });
+  }
+
+  await Promise.all([
+    Post.deleteMany({ _id: { $in: allPosts } }),
+    User.findByIdAndDelete(userId),
+    Session.findByIdAndDelete(req.headers.authorization),
+  ]);
+  return res.status(200).json({ message: "Deletion succesful" });
+});
 
 module.exports = router;
